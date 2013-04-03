42a283c9d4574c6907736b8b3a439ca7a1d79c2c
Adrian Lachata <iwtu@devrandom.sk>
2013-04-03 03:30:16 +0200

    tight-decoupling

---
+++ src/API/Auth/ITaskAPI.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Copyright © 2012 Adrián Lachata
+ * 
+ * CIIT! means Code It If Necessary. I use it when a piece of code have good reason,
+ * but it doesn't seem necassary to me.
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using TeamLab.API.Entities;
+
+
+namespace TeamLab.API.Auth
+{
+    interface ITaskAPI
+    {
+        public JTask[] MyTaskWithDetails();
+        public JTask GetTask(int taskid);
+        public JTask[] Tasks(int projectid);
+        public JTask[] MyTaskByStatus(string status);
+        public JTask[] MyTaskByProjectAndStatus(int projectid, Status status);
+        public JTask[] AllTask(int projectid);
+        public JTask[] TasksWithStatus(int projectid, Status status);
+        public void AddTAsk(int projectid, Dictionary<string, string> body);
+        JTask UpdateTask(int taskid, Dictionary<string, string> body);
+        public JTask UpdateTaskStatus(int taskid, Dictionary<string, string> body);
+    }
+}
